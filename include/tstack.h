5c2084d4f91d2de154ffc9c5c158837c042a9c2d
MilanaTarasova <150841920+miillta@users.noreply.github.com>
2024-04-09 16:50:25 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,37 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+    T stackarr[100];
+    int headIndex;
+
+ public:
+    TStack(): headIndex(-1) {}
+    void push(T value) {
+      if (!isFull())
+        stackarr[++headIndex] = value;
+      else
+        throw std::string("Full");
+    }
+    T get() const {
+      return stackarr[headIndex];
+    }
+    T pop() {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
+        return stackarr[headIndex--];
+    }
+    bool isEmpty() const {
+      return headIndex == -1;
+    }
+    bool isFull() const {
+      return headIndex == size - 1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
