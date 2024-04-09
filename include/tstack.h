2f668c63596438fe59ec2f0e355cba571812116c
MilanaTarasova <150841920+miillta@users.noreply.github.com>
2024-04-09 16:20:52 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,27 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+private: 
+	T* stackarr;
+	int index;
+public: 
+	TStack() : index(-1) {
+		stackarr = new T[size];
+	}
+	void pop() {
+		if (index >= 0)
+			index--;
+	}
+	void push(T item) {
+		if (index < size - 1)
+			stackarr[++index] = item;
+	}
+	T get() const {
+		return stackarr[index];
+	}
+	bool isEmpty() const {
+		return index == -1;
+	}
 };
 
 #endif  // INCLUDE_TSTACK_H_
